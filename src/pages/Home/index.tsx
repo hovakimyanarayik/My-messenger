e7783ecf1b041db7db070bdcac381d0b7c982ddb
hovakimyanarayik <hovaro1907@gmail.com>
2023-01-02 12:49:04 +0400

    fix bud

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,11 +2,21 @@ import './style.css'
 import React, { useEffect, useState } from "react";
 import Chats from './components/Chats';
 import OpenedChat from './components/OpenedChat';
+import { useParams } from 'react-router-dom';
 
 
 
 const Home: React.FC = () => {
     const [opened, setIsOpened] = useState<boolean>(true)
+    const params = useParams()
+
+    useEffect(() => {
+        if(params.slug) {
+            setIsOpened(false)
+        }
+
+        // eslint-disable-next-line
+    }, [params.slug])
 
     function handleCloseDrawer() {
         setIsOpened(false)
